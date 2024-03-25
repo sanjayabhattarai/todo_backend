bf3a95e786e8cb833ed6e31cc8dd47a8da064fd0
Sanjaya Bhattarai <t3bhsa00@students.oamk.fi>
2024-03-26 00:10:17 +0200

    project completed

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const {todoRouter} = require('./routes/todo.js');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', todoRouter);
+
+
+// Start the server
+const port = process.env.PORT;
+app.listen(port);
+    
